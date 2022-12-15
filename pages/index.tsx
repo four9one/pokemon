59a5e432f092d85744c0c11a089ab5dc46e37f9f
nicklas forsell <nicklas.forsell@cope.se>
2022-12-15 18:08:30 +0100

    new test counter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,70 +1,100 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next"
+import Head from "next/head"
+import Image from "next/image"
+import { useReducer, useState } from "react"
+
+const initialState = { count: 0 }
+// An interface for our actions
+interface CountAction {
+  type: string
+}
+
+// An interface for our state
+interface CountState {
+  count: number
+}
+
+function reducer(state: CountState, action: CountAction) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 }
+    case "decrement":
+      return { count: state.count - 1 }
+    case "reset":
+      return { count: 0 }
+    default:
+      throw new Error()
+  }
+}
 
 const Home: NextPage = () => {
+  const [counter, dispatch] = useReducer(reducer, initialState)
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Nivili</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+          Welcome to <div className="text-blue-600 inline-block">Nivili!</div>
         </h1>
 
         <p className="mt-3 text-2xl">
-          Get started by editing{' '}
+          Merged from stars from{" "}
           <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
+            /nivili.nu
           </code>
         </p>
+        <div className="rounded-md bg-blue-600 mt-4 text-zinc-200 p-4 w-44 text-xl">
+          {counter.count}
+        </div>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
+        <div className="mt-6 flex max-w-4xl flex-wrap items-center hover:[&>*]:cursor-pointer justify-around sm:w-full hover:[&>*]:bg-gray-400 hover:[&>*]:text-white">
+          <div
+            onClick={() => dispatch({ type: "increment" })}
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
+            <h3 className="text-2xl font-bold">Increase &rarr;</h3>
             <p className="mt-4 text-xl">
               Find in-depth information about Next.js features and its API.
             </p>
-          </a>
+          </div>
 
-          <a
-            href="https://nextjs.org/learn"
+          <div
+            onClick={() => dispatch({ type: "decrement" })}
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
+            <h3 className="text-2xl font-bold">Decrease &rarr;</h3>
             <p className="mt-4 text-xl">
               Learn about Next.js in an interactive course with quizzes!
             </p>
-          </a>
+          </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
+          <div
+            onClick={() => dispatch({ type: "reset" })}
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
+            <h3 className="text-2xl font-bold">Reset counter &rarr;</h3>
             <p className="mt-4 text-xl">
               Discover and deploy boilerplate example Next.js projects.
             </p>
-          </a>
+          </div>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          <div
+            onClick={() => {
+              setInterval(() => {
+                dispatch({ type: "increment" })
+              }, 1000)
+            }}
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
+            <h3 className="text-2xl font-bold">Start timer &rarr;</h3>
             <p className="mt-4 text-xl">
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
-          </a>
+          </div>
         </div>
       </main>
 
@@ -75,7 +105,7 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </a>
       </footer>
